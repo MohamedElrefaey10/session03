ef27893655cecf42a6d527f9ff687c580c1ee1af
MoGa <MoGa@DESKTOP-HSMA4DI>
2024-12-18 18:22:15 +0200

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -8,17 +8,33 @@ namespace Assignment
         {
             #region Q1
 
+            //Console.WriteLine("Enter Number");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number % 3 == 0 && number % 4 ==0)
+            //    Console.WriteLine("yes");
+            //else if (number % 4 == 0)
+            //    Console.WriteLine("yes");
+            //else
+            //    Console.WriteLine("No");
+
+
+
+            #endregion
+
+            #region Q2
+
             Console.WriteLine("Enter Number");
             int number = int.Parse(Console.ReadLine());
-
-            if (number % 3 == 0 && number % 4 ==0)
-                Console.WriteLine("yes");
-            else if (number % 4 == 0)
-                Console.WriteLine("yes");
-            else
-                Console.WriteLine("No");
-
-
+            switch (number)
+            {
+                case > 0:
+           Console.WriteLine("positive");
+                    break;
+                default:
+                    Console.WriteLine("negative");
+                    break;
+            }
 
             #endregion
         }
