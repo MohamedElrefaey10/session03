d96f9503003ba0aeced01ac4e7f7d79606bf36e6
MoGa <MoGa@DESKTOP-HSMA4DI>
2024-12-18 19:48:06 +0200

    Q4

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -39,23 +39,23 @@ namespace Assignment
             #endregion
 
             #region Q3
-            Console.WriteLine("Enter Number");
-            int A = int.Parse(Console.ReadLine());
-            int B = int.Parse(Console.ReadLine());
-            int C = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Number");
+            //int A = int.Parse(Console.ReadLine());
+            //int B = int.Parse(Console.ReadLine());
+            //int C = int.Parse(Console.ReadLine());
 
-            if (A > B && A > C)
-                Console.WriteLine($"max element {A}");
-            else if (B > A && B > C)
-                Console.WriteLine($"max element {B}");
-            else if (C > A && C > B)
-                Console.WriteLine($"max element {C}");
-            if (A < B && A < C)
-                Console.WriteLine($"min element {A}");
-            else if (B < A && B < C)
-                Console.WriteLine($"min element {B}");
-            else if (C < A && C < B)
-                Console.WriteLine($"min element {C}");
+            //if (A > B && A > C)
+            //    Console.WriteLine($"max element {A}");
+            //else if (B > A && B > C)
+            //    Console.WriteLine($"max element {B}");
+            //else if (C > A && C > B)
+            //    Console.WriteLine($"max element {C}");
+            //if (A < B && A < C)
+            //    Console.WriteLine($"min element {A}");
+            //else if (B < A && B < C)
+            //    Console.WriteLine($"min element {B}");
+            //else if (C < A && C < B)
+            //    Console.WriteLine($"min element {C}");
 
 
 
@@ -63,6 +63,21 @@ namespace Assignment
 
 
             #endregion
+
+            #region Q4
+
+            Console.WriteLine("Enter Number");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 3 == 0 && number % 4 == 0)
+                Console.WriteLine("yes");
+            else if (number % 4 == 0)
+                Console.WriteLine("Even number");
+            else
+                Console.WriteLine("Individual number");
+            #endregion
+
+
         }
     }
 }
