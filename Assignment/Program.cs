9bf559db77328f2dcd2a8079191c2fcd60a27872
MoGa <MoGa@DESKTOP-HSMA4DI>
2024-12-18 18:08:14 +0200

    Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Assignment
+﻿using System;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+
+            Console.WriteLine("Enter Number");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 3 == 0 && number % 4 ==0)
+                Console.WriteLine("yes");
+            else if (number % 4 == 0)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("No");
+
+
+
+            #endregion
         }
     }
 }
